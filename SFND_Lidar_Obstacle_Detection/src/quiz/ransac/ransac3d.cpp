d19d96e4c42be5630cae5fd80cbbddfca8bf4c59
fangkd <fangkd@umich.edu>
2020-04-11 22:12:24 -0400

    format

--- SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac3d.cpp
+++ SFND_Lidar_Obstacle_Detection/src/quiz/ransac/ransac3d.cpp
@@ -19,51 +19,47 @@ std::vector<float> cross(std::vector<float> v1, std::vector<float> v2);
 
 int main(){
   pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
-	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
+  // Create data
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 
 
   std::unordered_set<int> inliers = Ransac(cloud, 200, 0.2);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
 
-	for(int index = 0; index < cloud->points.size(); index++)
-	{
-		pcl::PointXYZ point = cloud->points[index];
-		if(inliers.count(index))
-			cloudInliers->points.push_back(point);
-		else
-			cloudOutliers->points.push_back(point);
-	}
+  for(int index = 0; index < cloud->points.size(); index++){
+    pcl::PointXYZ point = cloud->points[index];
+    if(inliers.count(index))
+      cloudInliers->points.push_back(point);
+    else
+      cloudOutliers->points.push_back(point);
+  }
 
 
-	// Render 2D point cloud with inliers and outliers
-	if(inliers.size())
-	{
-		renderPointCloud(viewer,cloudInliers,"inliers",Color(0,1,0));
-  	renderPointCloud(viewer,cloudOutliers,"outliers",Color(1,0,0));
-	}
-  else
-  {
-  	renderPointCloud(viewer,cloud,"data");
+  // Render 2D point cloud with inliers and outliers
+  if(inliers.size()){
+    renderPointCloud(viewer,cloudInliers,"inliers",Color(0,1,0));
+    renderPointCloud(viewer,cloudOutliers,"outliers",Color(1,0,0));
+  }
+  else{
+    renderPointCloud(viewer,cloud,"data");
   }
 	
-  while (!viewer->wasStopped ())
-  {
-  	viewer->spinOnce ();
+  while (!viewer->wasStopped ()){
+    viewer->spinOnce ();
   }
   return 0;
 }
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData3D(){
-	ProcessPointClouds<pcl::PointXYZ> pointProcessor;
-	return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
+  ProcessPointClouds<pcl::PointXYZ> pointProcessor;
+  return pointProcessor.loadPcd("../../../sensors/data/pcd/simpleHighway.pcd");
 }
 
 pcl::visualization::PCLVisualizer::Ptr initScene(){
-	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("2D Viewer"));
-	viewer->setBackgroundColor (0, 0, 0);
+  pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer ("2D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
   viewer->initCameraParameters();
   viewer->setCameraPosition(0, 0, 15, 0, 1, 0);
   viewer->addCoordinateSystem (1.0);
@@ -73,7 +69,7 @@ pcl::visualization::PCLVisualizer::Ptr initScene(){
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, 
                                int maxIterations, float distanceTol){
   std::unordered_set<int> inliersResult;
-	srand(time(NULL));
+  srand(time(NULL));
 
   int maxInliers = 0;
   while (maxIterations--){
